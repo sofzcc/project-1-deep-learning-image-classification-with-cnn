c20d5ab1af8ce7a598fc52a71a160d3f6da9add8
sofzcc <100369299+sofzcc@users.noreply.github.com>
2024-07-24 15:29:08 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -59,4 +59,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the port from the environment variable or default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)

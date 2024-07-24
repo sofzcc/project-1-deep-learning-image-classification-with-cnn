084096bc1464595d169615037aaf5dc56e503e16
sofzcc <100369299+sofzcc@users.noreply.github.com>
2024-07-24 18:24:47 +0200

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Start TensorFlow Serving
-tensorflow_model_server --rest_api_port=8501 --model_name=${MODEL_NAME} --model_base_path=/models/model &
+# Start Flask app in the background
+python3 /app/app.py &
 
-# Start Flask app
-python3 /app.py
+# Start TensorFlow Serving
+tensorflow_model_server --rest_api_port=8501 --model_name=model --model_base_path=/models/model
+Make sure to give it execute permissions:

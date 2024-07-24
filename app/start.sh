a2eee17a767ee83bf14e5f28814689dd288a3657
sofzcc <100369299+sofzcc@users.noreply.github.com>
2024-07-24 14:52:24 +0200

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Start Flask app in the background
-python3 /app.py &
-
 # Start TensorFlow Serving
-tensorflow_model_server --rest_api_port=8501 --model_name=${MODEL_NAME} --model_base_path=/models/model
+tensorflow_model_server --rest_api_port=8501 --model_name=${MODEL_NAME} --model_base_path=/models/model &
+
+# Start Flask app
+python3 /app.py
